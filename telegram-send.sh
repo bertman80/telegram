fe92572493372fb06a8d11f3fedf2039d0cb5684
bert <49372063+bertman80@users.noreply.github.com>
2021-12-15 18:47:28 +0100

    Update telegram-send.sh

--- telegram-send.sh
+++ telegram-send.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
-    
-GROUP_ID=<group_id>
-BOT_TOKEN=<bot_token>
+# telegram basic send module
+# this script just send 
+# location file: 
+#     sudo cp telegram_send /usr/bin/telegram-send
+#     sudo chmod +x /usr/bin/telegram-send
+# Group_id is number from the reveived user or group
+# Bot_token is number you get when telegramuser is created
+
+GROUP_ID=<GROUP_ID> 
+BOT_TOKEN=<BOT_TOKEN>
 
 # this 3 checks (if) are not necessary but should be convenient
 if [ "$1" == "-h" ]; then
+  # Help
+  echo ""
+  echo "Help `basename $0`"
   echo "Usage: `basename $0` \"text message\""
+  echo ""
   exit 0
 fi
 
 if [ -z "$1" ]
   then
-    echo "Add message text as second arguments"
+    # Missing text
+    echo ""
+    echo "Error: Missing argument"
+    echo "Usage: `basename $0` \"text message\""
+    echo ""
     exit 0
 fi
 
 if [ "$#" -ne 1 ]; then
-    echo "You can pass only one argument. For string with spaces put it on quotes"
+    # Multiple arguments
+    echo ""
+    echo "Error: One argument allowed."
+    echo "Usage: `basename $0` \"text message\""
+    echo ""
     exit 0
 fi
 
